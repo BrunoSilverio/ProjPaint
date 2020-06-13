3276b8310a353bf4542318aa491850fb33c6f9cd
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-06-12 22:09:27 -0300

    tentando arrumar cliente servidor

---
+++ Servidor/Desenho.java
@@ -0,0 +1,26 @@
+package Servidor;
+
+import java.util.Vector;
+
+import Cliente.Figura;
+
+//Contem todos os desenhos
+public class Desenho {
+	private Vector<Figura> desenhos;
+
+    public Desenho (double valorResultante)
+    {
+        //this.valorResultante = valorResultante;
+    }
+
+    /*public double getValorResultante ()
+    {
+        //return this.valorResultante;
+    }
+    
+    public String toString ()
+    {
+		//return (""+this.valorResultante);
+	}*/
+    //metodo adicionaDesenho - pegar no BD e mandar pro cliente
+}
