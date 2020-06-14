189c0fd0bfdbbe997e3cecc997b9e744012c4280
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-06-14 01:03:09 -0300

    cliente branch augusto + ajustes servidor

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -89,70 +89,7 @@ public class Cliente
         char opcao=' ';
         do
         {
-            System.out.print ("Sua opcao (+, -, *, /, =, [T]erminar)? ");
-
-            try
-            {
-				//opcao = Character.toUpperCase(Teclado.getUmChar());
-		    }
-		    catch (Exception erro)
-		    {
-				System.err.println ("Opcao invalida!\n");
-				continue;
-			}
-
-			if ("+-*/=T".indexOf(opcao)==-1)
-		    {
-				System.err.println ("Opcao invalida!\n");
-				continue;
-			}
-
-			try
-			{
-				double valor=0;
-				if ("+-*/".indexOf(opcao)!=-1)
-				{
-					System.out.print ("Valor? ");
-					try
-					{
-						//valor = Teclado.getUmDouble();
-						System.out.println();
-						
-						if (opcao=='/' && valor==0)
-						{
-							System.err.println ("Valor invalido!\n");
-							continue;
-						}
-					}
-					catch (Exception erro)
-					{
-						System.err.println ("Valor invalido!\n");
-						continue;
-					}
-
-
-					servidor.receba (new PedidoDeOperacao (opcao,valor));
-				}
-				else if (opcao=='=')
-				{
-					servidor.receba (new PedidoDeResultado ());
-					Comunicado comunicado = null;
-					do
-					{
-						comunicado = (Comunicado)servidor.espie ();
-					}
-					while (!(comunicado instanceof Resultado));
-					Resultado resultado = (Resultado)servidor.envie ();
-					System.out.println ("Resultado atual: "+resultado.getValorResultante()+"\n");
-				}
-			}
-			catch (Exception erro)
-			{
-				System.err.println ("Erro de comunicacao com o servidor;");
-				System.err.println ("Tente novamente!");
-				System.err.println ("Caso o erro persista, termine o programa");
-				System.err.println ("e volte a tentar mais tarde!\n");
-			}
+            new Editor ();
         }
         while (opcao != 'T');
 
